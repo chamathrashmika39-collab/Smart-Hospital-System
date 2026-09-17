ed85e329b85a1756b31a0fb64c92074523820bd4
Chamath Rashmika <chamathrashmika39@gmail.com>
2026-09-17 10:13:25 +0530

    Initial Project Setup

---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("----------------------------------SMART HOSPITAL PATIENT & RESOURCE ALLOCATION SYSTEM----------------------------------\n");
+    return 0;
+}
