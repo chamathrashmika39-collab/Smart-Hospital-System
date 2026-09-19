e7ac097cd890f8de6342f162bfc2d0d982f8aadc
Chamath Rashmika <chamathrashmika39@gmail.com>
2026-09-19 13:33:32 +0530

    Added Specialty and Ward Data

--- main.c
+++ main.c
@@ -3,6 +3,12 @@
 
 int main()
 {
+    int specialtyFee[4] = {1500, 2500, 4500, 5000};
+    int specialtyTime[4] = {15, 20, 30, 30};
+    int specialtyCapacity[4] = {30, 20, 12, 10};
+    int wardRate[4] = {3000, 6000, 12000, 25000};
+    int wardCapacity[4] = {20, 10, 10, 5};
     printf("----------------------------------SMART HOSPITAL PATIENT & RESOURCE ALLOCATION SYSTEM----------------------------------\n");
+
     return 0;
 }
