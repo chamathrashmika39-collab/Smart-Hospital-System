86a970caf401bd787a33c1c412ba3d26346e712b
Chamath Rashmika <chamathrashmika39@gmail.com>
2026-09-19 14:31:35 +0530

    Added Hospital Information Display

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void displayHospitalInformation();
+
 int main()
 {
     int specialtyFee[4] = {1500, 2500, 4500, 5000};
@@ -9,6 +11,45 @@ int main()
     int wardRate[4] = {3000, 6000, 12000, 25000};
     int wardCapacity[4] = {20, 10, 10, 5};
     printf("----------------------------------SMART HOSPITAL PATIENT & RESOURCE ALLOCATION SYSTEM----------------------------------\n");
+    displayHospitalInformation();
 
     return 0;
 }
+
+void displayHospitalInformation()
+{
+    printf("\nSPECIALTIES\n");
+
+    for (int i = 0; i < 4; i++)
+    {
+        printf("%d. ", i + 1);
+
+        if (i == 0)
+            printf("General Practice\n");
+        else if (i == 1)
+            printf("Paediatrics\n");
+        else if (i == 2)
+            printf("Cardiology\n");
+        else
+            printf("Neurology\n");
+
+    }
+
+    printf("\nWARDS\n");
+
+    for (int i = 0; i < 4; i++)
+    {
+        printf("%d. ", i + 1);
+
+        if (i == 0)
+            printf("General Ward\n");
+        else if (i == 1)
+            printf("Paediatric Ward\n");
+        else if (i == 2)
+            printf("Surgical Ward\n");
+        else
+            printf("ICU\n");
+    }
+}
+
+
